2c99a81303acb228cbaf78d9d78c293fb6fb6127
salvijaison28 <salvijaison28@gmail.com>
2025-11-16 17:59:28 +0000

    Deploy updated portfolio with blog + redux

--- .history/src/main_20251116174345.tsx
+++ .history/src/main_20251116174345.tsx
@@ -3,12 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import "./index.css"; // ← keep if you have global CSS
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
